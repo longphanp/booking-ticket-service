c427fb1e16552e451f17b2d6a518b1c8f95c3ec8
longphanp <phanlonghd5200@gmail.com>
2024-05-19 16:24:00 +0700

    fix: deploy to railway

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import configs from "configs";
 import express from "express";
 import { logger } from "logger";
 import initMiddlewares from "middlewares";
+import initResources from "resources";
 import initRouters from "routers";
 
 const app = express();
@@ -10,5 +11,6 @@ initMiddlewares(app);
 initRouters(app);
 
 app.listen(configs.port, () => {
+  initResources();
   logger.info(`App listen at port ${configs.port}`);
 });
