4d616e3118c31799291549aac36a6088fafa541e
longphanp <phanlonghd5200@gmail.com>
2024-05-19 16:02:30 +0700

    fix: add configs route

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,3 +1,4 @@
+import configs from "configs";
 import { Express } from "express";
 
 import v1 from "./v1";
@@ -5,6 +6,10 @@ import v1 from "./v1";
 export default function initRouters(app: Express) {
   app.get("/", (_, res) => {
     res.send("PONG");
+  });
+
+  app.get("/configs", (_, res) => {
+    res.json(configs);
   });
 
   app.use("/v1", v1);
