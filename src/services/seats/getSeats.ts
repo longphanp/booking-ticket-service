488225d4e97f0e7798fd35a922b3e44f78046be2
longphanp <phanlonghd5200@gmail.com>
2024-05-21 07:31:41 +0700

    fix: update get seats query

--- src/services/seats/getSeats.ts
+++ src/services/seats/getSeats.ts
@@ -4,7 +4,7 @@ import SeatModel from "models/Seat";
 
 export default async function getSeats(query?: QuerySeatDTO) {
   const { areaId, size = DEFAULT_SIZE, offset = DEFAULT_OFFSET } = query!;
-  const data = await SeatModel.find({ ...(areaId && { areaId }) })
+  const data = await SeatModel.find({ ...(areaId && { area: areaId }) })
     .populate("area")
     .limit(size)
     .skip(offset);
